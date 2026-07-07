4943a2b8604a08006668d260b586b777e0e68b90
Richelle Mendez <richellemendez35@gmail.com>
2026-07-07 23:09:34 +0800

    fixing color

--- app/src/main/java/com/example/budgettracker/ui/theme/Theme.kt
+++ app/src/main/java/com/example/budgettracker/ui/theme/Theme.kt
@@ -21,8 +21,6 @@ private val DarkColorScheme = darkColorScheme(
     error = ExpenseRed
 )
 
-
-
 private val LightColorScheme = lightColorScheme(
     primary = PrimaryBrown,
     secondary = SecondaryCream,
